cd181a6b97fdd4f07303fd8c6683a5dced761f4a
Dale Lukas Peterson <hazelnusse@gmail.com>
2012-08-10 12:06:15 -0700

    Scrubbed comments; mask high bits of CCR[i] on read.

--- Interrupts.cpp
+++ Interrupts.cpp
@@ -21,9 +21,8 @@ CH_IRQ_HANDLER(VectorB8)
   static uint32_t CCR_prev[3] = { 0, 0, 0 };  // to store the value of CNT at
                                               // the time of the input capture
                                               // events
-  static uint32_t pre_overflow_cnt[3] = {0, 0, 0};
+  static uint32_t pre_overflow_cnt[3] = {0, 0, 0};  // used to store the count
 
-  // uint32_t cnt = STM32_TIM4->CNT; // Save TIM4 count
   uint16_t sr = STM32_TIM4->SR;   // Save TIM4 status register
   uint32_t dir = GPIOA->IDR;      // Save GPIOA; encoders B channel state
   STM32_TIM4->SR = ~sr;           // Write zero to bits high at time of SR read
@@ -39,41 +38,33 @@ CH_IRQ_HANDLER(VectorB8)
     // 
     // 1) This is the first overflow (overflows[i] == 0) to occur since the
     // last IC interrupt.  In this case, we need store 2^16 minus the value of
-    // CNT when the last IC interrupt occurred.  We then increment overflows.
+    // CNT when the last IC interrupt occurred.  We then increment overflows[i].
     //
     // 2) This is not the first overflow (overflows[i] > 0) to occur since the
     // last IC interrupt.  In this case, we simply increment overflows[i] and
     // leave pre_overflow_cnt[i] untouched.
     //
     // In both cases, we increment overflows[i], so this can be all wrapped
-    // into a single for loop:
-    
+    // into a for loop:
     for (int i = 0; i < 3; ++i) {
-      if (overflows[i]++ == 0)  // check overflows before incrementing
+      if (overflows[i]++ == 0) { // check overflows before incrementing
         pre_overflow_cnt[i] = (1 << 16) - CCR_prev[i];
+      }
     }
   }
 
-  // For IC1, IC2, IC3 (i = 0, 1, 2) events we need to do the following:
-  // 1) If we haven't overflowed the counter, the Clockticks is simply the
-  // clock value at the time of the IC event, CCR[i], minus the clock value at
-  // the time of the previous event, CCR_prev[i].  If we have overflowed the
-  // counter, the Clockticks is: 
-  // pre_overflow_cnt[i] + (overflows[i] - 1)*2^16 + CCR[i],
-  // 2) overflows[i] = 0;
-  // 4) Save state of Encoder B; this indicates the direction:
-  //    if (dir & (1 << (i + 12)))
-  //      SampleAndControl::timers.Direction[i] = EncoderTimers::cw;
-  //    else
-  //      SampleAndControl::timers.Direction[i] = EncoderTimers::ccw;
-  //    
-  // All of this can be wrapped into a nice for loop:
-  
   for (int i = 0; i < 3; ++i) {
+    // For IC1, IC2, IC3 events (i = 0, 1, 2) we need to do the following: 1)
+    // If we haven't overflowed the counter, the Clockticks is the clock
+    // value at the time of the IC event, CCR[i], minus the clock value at the
+    // time of the previous event, CCR_prev[i].  If we have overflowed the
+    // counter, the Clockticks is: pre_overflow_cnt[i] + (overflows[i] -
+    // 1)*2^16 + CCR[i]. After this, we need to set overflows[i] = 0.  Finally,
+    // we determine direct by looking at the state of the Encoder B line.
     if (sr & (1 << (i + 1))) { // IC1, IC2, IC3 are on bits 1, 2, 3 of SR
-
       // Clock ticks since last rising edge
-      uint32_t tmp = STM32_TIM4->CCR[i]; // obtain count since last overflow
+      uint32_t tmp = STM32_TIM4->CCR[i]; // Timer count at time of rising edge
+      tmp &= 0x0000FFFF;                 // Mask out top bits, just to be safe
 
       if (overflows[i] == 0) {
         SampleAndControl::timers.Clockticks[i] = tmp - CCR_prev[i];
@@ -85,10 +76,10 @@ CH_IRQ_HANDLER(VectorB8)
       }
       CCR_prev[i] = tmp;      // save the capture compare register
 
-      // Direction
-      if (dir & (1 << i))     // Encoder B lines are on PA0, PA1, PA2
+      // Direction, input is on GPIOA, pins 0, 1, 2
+      if (dir & (1 << i))     // Encoder B line is high
         SampleAndControl::timers.Direction[i] = EncoderTimers::cw;
-      else                    // Enocder B line is low
+      else                    // Encoder B line is low
         SampleAndControl::timers.Direction[i] = EncoderTimers::ccw;
     }
   }
