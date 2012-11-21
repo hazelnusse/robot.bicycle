fe9f015ba08ad740bf68b903e3bd69045d991669
Dale Lukas Peterson <hazelnusse@gmail.com>
2012-11-21 02:26:30 -0800

    Added sample converter.cc

---
+++ convertsamples.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+
+#include "SampleReader.h"
+#include "SampleConverted.h"
+
+int main(int argc, char *argv[])
+{
+  if (argc != 3) {
+    std::cout << "usage: " << argv[0] << " <input data filename> <output data filename>" << std::endl;
+    return 0;
+  }
+  SampleReader sr(argv[1]);
+  std::vector<SampleConverted> samples(sr.Convert());
+  std::cout << samples.size() << " samples converted @ "
+                              << sizeof(SampleConverted)
+                              << " bytes per sample " << std::endl;
+
+  std::ofstream outfile(argv[2], std::ios_base::out | std::ios_base::binary);
+  for (int i; i < samples.size(); ++i) {
+    outfile.write(reinterpret_cast<char *>(&samples[i]), sizeof(SampleConverted));
+  }
+  outfile.close();
+}
