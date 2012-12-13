f751e6ad2e9a47a74025b6d0f2b1ef239e16bd88
Dale Lukas Peterson <hazelnusse@gmail.com>
2012-12-13 01:42:17 -0800

    Removed VectorEO

--- Interrupts.cpp
+++ Interrupts.cpp
@@ -5,16 +5,6 @@
 extern "C" {
 #endif
 
-// Vector 0x00E0 is called on a rising/falling edge of the steer index
-// Need to save the steer encoder count and determine the direction, which can
-// be obtained from STM32_TIM3->CNT and STM32_TIM3->CR1[4]  (DIR bit)
-CH_IRQ_HANDLER(VectorE0)
-{
-  CH_IRQ_PROLOGUE();
-
-  CH_IRQ_EPILOGUE();
-}
-
 #ifdef __cplusplus
 }
 #endif
