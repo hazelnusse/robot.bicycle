394a2208abc560cfa93a7abb4af073597072b4ed
Dale Lukas Peterson <hazelnusse@gmail.com>
2013-01-17 00:51:51 -0800

    Added rate gyro calcs, implemented cross-sensitivities.

--- imu_calibration.py
+++ imu_calibration.py
@@ -15,13 +15,11 @@ g = symbols('g')
 
 # Unknown scale factors and biases
 sxx, syy, szz = symbols('sxx syy szz')
-sxy, sxz, syx, syz, szx, szy = symbols('sxy sxz syx syz szx szy')
+sxy, syz, sxz = symbols('sxy syz sxz')
 bx, by, bz = symbols('bx by bz')
-s = Matrix([[sxx, sxy, sxz], [syx, syy, syz], [szx, szy, szz]])
+s = Matrix([[sxx, sxy, sxz], [sxy, syy, syz], [sxz, syz, szz]])
 b = Matrix([[bx], [by], [bz]])
-no_cross_axis_sensitivity = {sxy : 0.0, sxz : 0.0, syx : 0.0, syz : 0.0, szx :
-        0.0, szy : 0.0}
-
+no_cross_axis_sensitivity = {sxy : 0.0, sxz : 0.0, syz : 0.0}
 # Inertial frame
 N = ReferenceFrame('N')
 
@@ -57,65 +55,72 @@ configurations = [{phi : 0, theta : 0,
                               datadir + "dsideup02_converted.dat"]}]
 
 eqns = zeros(18,1)
+w_mean = np.zeros((12,3))
 for i, configuration in enumerate(configurations):
     p = configuration[phi]
     t = configuration[theta]
     d01 = np.fromfile(configuration['files'][0], dtype=st.sample_t)
     d02 = np.fromfile(configuration['files'][1], dtype=st.sample_t)
-    a01_mean = denormalize(np.array([d01['accx'], d01['accy'],
-        d01['accz']]).mean(axis=1).T)
-    a02_mean = denormalize(np.array([d02['accx'], d02['accy'],
-        d02['accz']]).mean(axis=1).T)
-    a_measured = Matrix((a01_mean + a02_mean)/2).T
+    a01_mean = denormalize(np.array([d01['accx'],
+                                     d01['accy'],
+                                     d01['accz']]).mean(axis=1).T)
+    a02_mean = denormalize(np.array([d02['accx'],
+                                     d02['accy'],
+                                     d02['accz']]).mean(axis=1).T)
+    a_measured = Matrix((a01_mean + a02_mean)/2.0).T
     a_sym_sensor = Matrix([(-g*A.z & si).subs({g : 9.81, phi : p, theta : t}) for si in S])
     eqns[3*i], eqns[3*i + 1], eqns[3*i + 2] = a_sym_sensor - s*a_measured - b
 
-unknowns = [phi_s, theta_s, psi_s, sxx, sxy, sxz, syx, syy, syz, szx, szy, szz,
-        bx, by, bz]
+    # Checking rate gyroscope signals
+    w_mean[2*i + 0, :] = np.array([d01['gyrox'], d01['gyroy'], d01['gyroz']])[:,10:].mean(axis=1).T
+    w_mean[2*i + 1, :] = np.array([d02['gyrox'], d02['gyroy'], d02['gyroz']])[:,10:].mean(axis=1).T
+
+print("Body fixed angular velocity means for each run:")
+print(w_mean)
+print("Rate gyroscope sample means:")
+print(w_mean.mean(axis=0))
+print("Rate gyroscope sample standard deviations:")
+print(w_mean.std(axis=0))
+
+unknowns = [phi_s, theta_s, psi_s, sxx, syy, szz, sxy, syz, sxz, bx, by, bz]
 eqns_dx = eqns.jacobian(unknowns)
 
 def residual(x):
-    return np.array(eqns.subs(dict(zip(unknowns,
-        x))).subs(no_cross_axis_sensitivity).T.tolist()[0], np.float64)
-    #return np.array(eqns.subs({phi_s : x[0], theta_s : x[1], psi_s : x[2],
-    #                  sxx : x[3], sxy : 0.0, sxz : 0.0,
-    #                  syx : 0.0, syy : x[7], syz : 0.0,
-    #                  szx : 0.0, szy : 0.0, szz : x[11],
-    #                  bx : x[12], by : x[13], bz : x[14]}).T.tolist()[0],
-    #                  np.float64)
+    return np.array(eqns.subs(dict(zip(unknowns, x))).T.tolist()[0], np.float64)
 
 def jacobian(x):
-    df = np.array(eqns_dx.subs(dict(zip(unknowns, x))).subs(no_cross_axis_sensitivity).tolist(), np.float64)
-    z = np.zeros((18,))
-    df[:, 4] = z
-    df[:, 5] = z
-    df[:, 6] = z
-    df[:, 8] = z
-    df[:, 9] = z
-    df[:, 10] = z
-    return df
+    return np.array(eqns_dx.subs(dict(zip(unknowns, x))).tolist(), np.float64)
 
 s_nom = 9.81 / 16384.0
 print("nominal sensitivity = {0}".format(s_nom))
-x0 = [0.0, 0.0, 0.0,  # orientation angles
-      s_nom, 0., 0.,           # x-axis sensitivities
-      0., s_nom, 0.,           # y-axis sensitivities
-      0., 0., s_nom,           # z-axis sensitivities
-      0., 0., 0.]              # biases
+x0 = [0.0, 0.0, 0.0,           # orientation angles
+      s_nom, s_nom, s_nom,     # main x,y,z sensitivities
+      0.0, 0.0, 0.0,           # xy, yz, xz cross-axis sensitivities
+      0.0, 0.0, 0.0]           # biases
 
 x, cov_x, infodict, mesg, ier = so.leastsq(residual,
                                            x0,
                                            Dfun=jacobian,
-                                           maxfev=1000,
+                                           maxfev=100,
                                            ftol=1e-16,
                                            xtol=1e-16,
                                            full_output=True)
-print("Solution:\n", x)
-print("Covariance:\n", cov_x)
-print("Information:\n", infodict)
-print("Message:\n", mesg)
-print("Iterations:\n", ier)
+#print("Solution:\n", x)
+#print("Covariance:\n", cov_x)
+#print("Information:\n", infodict)
+#print("Message:\n", mesg)
+#print("MINPACK INFO:", ier)
+
+print("Bicycle frame to sensor frame Euler XZY - (-pi/2 + phi_s, pi + theta_s, psi_s) angles")
+print("phi_s: {0} rad [{1} deg]".format(x[0], x[0]*180/np.pi))
+print("theta_s: {0} rad [{1} deg]".format(x[1], x[1]*180/np.pi))
+print("psi_s: {0} rad [{1} deg]".format(x[2], x[2]*180/np.pi))
+
+s_numerical = np.array([[x[3], x[6], x[8]],[x[6], x[4], x[7]], [x[8], x[7], x[5]]])
+print("Sensitivity Matrix, m/s^2/bit:\n", s_numerical)
+print("Biases, m/s^2:\n", x[9:])
+
+print("Residuals, m/s^2:\n", infodict['fvec'])
 
 sensor_orienation_angles = {phi_s : x[0], theta_s : x[1], psi_s : x[2]}
-
-print("Direction cosine matrix:\n", C.dcm(S).subs(sensor_orienation_angles))
+print("Direction cosine matrix (S to C):\n", C.dcm(S).subs(sensor_orienation_angles))
