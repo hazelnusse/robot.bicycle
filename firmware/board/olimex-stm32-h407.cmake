e096d65be348d819f04dcaf9428855487b706556
Oliver Lee <oliverzlee@gmail.com>
2013-04-17 14:43:31 -0700

    Change CMake options to use FP hardware.

--- firmware/board/olimex-stm32-h407.cmake
+++ firmware/board/olimex-stm32-h407.cmake
@@ -2,7 +2,8 @@
 
 # platform files and flags
 include(${CHIBIOS_CMAKE_DIR}/platforms/stm32-f4xx.cmake)
-include(${CHIBIOS_CMAKE_DIR}/ports/gcc/stm32-f4xx.cmake)
+#include(${CHIBIOS_CMAKE_DIR}/ports/gcc/stm32-f4xx.cmake)
+include(${ROBOT_BICYCLE_SOURCE_DIR}/stm32-f4xx.cmake)
 
 # path to the board files
 get_filename_component(CHIBIOS_BOARD_PATH "${CMAKE_CURRENT_LIST_FILE}" PATH)
---
+++ firmware/stm32-f4xx.cmake
@@ -0,0 +1,36 @@
+
+# gcc ARMCMx common
+include(${CHIBIOS_CMAKE_DIR}/ports/gcc/armcmx.cmake)
+
+# source files
+set(CHIBIOS_SRC
+    ${CHIBIOS_SRC}
+    ${CHIBIOS_ROOT}/os/ports/GCC/ARMCMx/STM32F4xx/cmparams.h
+    ${CHIBIOS_ROOT}/os/ports/GCC/ARMCMx/STM32F4xx/vectors.c
+)
+include_directories(
+    ${CHIBIOS_ROOT}/os/ports/GCC/ARMCMx/STM32F4xx
+)
+
+# set the type of CPU
+set(CPU_FLAGS "-mcpu=cortex-m4 -mthumb")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${CPU_FLAGS}")
+set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${CPU_FLAGS}")
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${CPU_FLAGS} -nostartfiles")
+
+# set the linker script
+set(DEFAULT_LINKER_SCRIPT "${ROBOT_BICYCLE_SOURCE_DIR}/STM32F407xG_ram_vectors.ld")
+
+# option to use the hardware FPU
+option(CHIBIOS_USE_HARDWARE_FPU "Use hardware FPU" TRUE)
+if(CHIBIOS_USE_HARDWARE_FPU)
+    add_definitions("-DCORTEX_USE_FPU=TRUE")
+    set(FPU_FLAGS "-march=armv7e-m -mfloat-abi=hard -mfpu=fpv4-sp-d16 -fsingle-precision-constant")
+    set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${FPU_FLAGS}")
+    set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${FPU_FLAGS}")
+    # Add path to linker flags so CMake will find the right libraies
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} ${FPU_FLAGS} -L${TOOLCHAIN_DIR}/${TOOLCHAIN}/lib/armv7e-m/fpu/")
+else()
+    add_definitions("-DCORTEX_USE_FPU=FALSE")
+endif()
+
