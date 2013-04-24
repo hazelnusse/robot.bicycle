63461e88aef3258c698ebb49353d8dd0ede94ffe
Dale Lukas Peterson <hazelnusse@gmail.com>
2013-04-24 13:58:31 -0700

    ChibiOS Makefile changes.

--- firmware/build.sh
+++ firmware/build.sh
@@ -3,5 +3,7 @@
 # USE_FPU in accordence with whether your target MCU has a FPU or not.
 protoc -I/usr/include -Inanopb/generator -I. -o Sample.pb Sample.proto 
 python2 ./nanopb/generator/nanopb_generator.py Sample.pb
-mv Sample.pb Sample.pb.h Sample.pb.c ./build
+mv Sample.pb.h Sample.pb.c ./src
 make -j8 USE_FPU=yes USE_VERBOSE_COMPILE=yes TRGT=~/toolchain/bin/arm-none-eabi-
+rm ./src/Sample.pb.c ./src/Sample.pb.h ./Sample.pb
+
