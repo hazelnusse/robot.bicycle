e411a866e57887fa3785f86a013dadc939a70576
Dale Lukas Peterson <hazelnusse@gmail.com>
2013-04-24 02:03:20 -0700

    Updated Makefile to build project with protobuf

--- firmware/build.sh
+++ firmware/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # To build the firmware, adjust TRGT to point to your toolchain, and set
 # USE_FPU in accordence with whether your target MCU has a FPU or not.
-protoc -o Test.pb Test.proto
-python2 ./nanopb/generator/nanopb_generator.py Test.pb
+protoc -I/usr/include -Inanopb/generator -I. -o Sample.pb Sample.proto 
+python2 ./nanopb/generator/nanopb_generator.py Sample.pb
+mv Sample.pb Sample.pb.h Sample.pb.c ./build
 make -j8 USE_FPU=yes USE_VERBOSE_COMPILE=yes TRGT=~/toolchain/bin/arm-none-eabi-
