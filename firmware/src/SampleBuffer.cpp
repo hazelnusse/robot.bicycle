06592ced13068be54b8d5472fc7f8c323bbbb03f
Oliver Lee <oliverzlee@gmail.com>
2013-05-01 22:18:55 -0700

    Use a semaphore for buffer management.
    Replace mutex with semaphore in the manage and write threads. Stop
    writing samples when all buffers are filled.

--- firmware/src/SampleBuffer.cpp
+++ firmware/src/SampleBuffer.cpp
@@ -8,7 +8,7 @@ const uint16_t SampleBuffer::blocks_per_buffer_;
 const uint8_t SampleBuffer::number_of_buffers_;
 
 SampleBuffer::SampleBuffer()
-  : tp_write_(0), tp_manage_(0), active_buffer_(0)
+  : tp_write_(0), tp_manage_(0)
 {
 }
 
@@ -31,53 +31,80 @@ msg_t SampleBuffer::deinitialize()
   return write_errors;
 }
 
-msg_t SampleBuffer::manager_thread(void * filename)
+msg_t SampleBuffer::manager_thread(const char *filename)
 {
   chRegSetThreadName("manage");
-  FRESULT res = f_open(&f_, static_cast<char *>(filename),
-                       FA_WRITE | FA_CREATE_ALWAYS);
+  FRESULT res = f_open(&f_, filename, FA_WRITE | FA_CREATE_ALWAYS);
   if (res != FR_OK)
     chThdExit(systemstate::FATFS_f_open_error);
 
-  chMtxInit(&buffer_mtx_);
+  chSemInit(&buffer_sem_, 0);
+  uint8_t active_buffer = 0;
 
   // Spawn the write thread
   tp_write_ = chThdCreateStatic(waWriteThread,
                                 sizeof(waWriteThread),
                                 NORMALPRIO + 1,
                                 write_thread_,
-                                NULL);
+                                &active_buffer);
 
   uint16_t buffer_index = 0;
+  uint16_t excess_bytes = 0;
   while (1) {
-    Thread * tp = chMsgWait(); 
+    Thread * tp = chMsgWait();
     msg_t m = chMsgGet(tp);
     if (m == -1) {
       chMsgRelease(tp, 0);
       break;
     }
-    pb_ostream_t out = pb_ostream_from_buffer(
-        &buffer_[active_buffer_][buffer_index + 2],
-        buffer_[active_buffer_].size() - buffer_index - 2);
-    Sample * s = reinterpret_cast<Sample *>(m);
-    chMsgRelease(tp, pb_encode(&out, Sample_fields, s));
 
-    buffer_[active_buffer_][buffer_index] = out.bytes_written;           // LSB
-    buffer_[active_buffer_][buffer_index + 1] = out.bytes_written >> 8;  // MSB
-    uint16_t packet_size = out.bytes_written + 2;
+    //     A    B   C
+    //   --------------
+    //   | -1 | 0 | 1 |
+    //   --------------
+    // When using three buffers, if the manage thread is filling buffer A and the
+    // write thread is waiting for buffer A to be filled, sem counter is -1.
+    // When buffer A is filled and the manage thread is filling buffer B, the
+    // sem counter is increased to 0, and the write thread is resumed. If buffer
+    // B is filled, manage thread switches to buffer C, and increments the sem
+    // counter. If the write thread is still writing data from buffer A, we want
+    // increment the sem counter and set the active buffer for the manage thread 
+    // to buffer A, but prevent it from encoding any samples.
+    if (buffer_overflow()) {
+      chMsgRelease(tp, false); // pb_encode returns false on failure
+      continue;
+    }
 
-    if (buffer_index + packet_size < bytes_per_buffer_) {
-      buffer_index += packet_size;
-    } else {  // we've filled or overflowed the buffer
-      uint16_t excess_bytes = buffer_index + packet_size  - bytes_per_buffer_;
-      uint8_t next_buffer = (active_buffer_ + 1) % number_of_buffers_;
-      memcpy(&buffer_[next_buffer][0],
-             &buffer_[active_buffer_][bytes_per_buffer_],
+    // if we aren't overflowing, but we weren't able to copy over excess bytes at
+    // the previous time, ignore the new sample and copy over excess bytes.
+    if (excess_bytes > 0) {
+      chMsgRelease(tp, false);
+    } else {
+      pb_ostream_t out = pb_ostream_from_buffer(
+          &buffer_[active_buffer][buffer_index + 2],
+          buffer_[active_buffer].size() - buffer_index - 2);
+      Sample * s = reinterpret_cast<Sample *>(m);
+      chMsgRelease(tp, pb_encode(&out, Sample_fields, s));
+
+      buffer_[active_buffer][buffer_index] = out.bytes_written & 0xff;    // LSB
+      buffer_[active_buffer][buffer_index + 1] = out.bytes_written >> 8;  // MSB
+      buffer_index += out.bytes_written + 2;
+
+      if (buffer_index >= bytes_per_buffer_) {
+        chSemSignal(&buffer_sem_);
+        excess_bytes = buffer_index - bytes_per_buffer_;
+        buffer_index = excess_bytes;
+        active_buffer = (active_buffer + 1) % number_of_buffers_;
+      }
+    }
+
+    if (!buffer_overflow() && (excess_bytes > 0)) {
+      uint8_t prev_buffer = ((active_buffer + number_of_buffers_ - 1) %
+                             number_of_buffers_);
+      memcpy(&buffer_[active_buffer][0],
+             &buffer_[prev_buffer][bytes_per_buffer_],
              excess_bytes);
-      buffer_index = excess_bytes;
-      chMtxLock(&buffer_mtx_);
-        active_buffer_ = next_buffer;
-      chMtxUnlock();
+      excess_bytes = 0;
     }
   }
   chThdTerminate(tp_write_);        // request that write thread terminate
@@ -89,29 +116,23 @@ msg_t SampleBuffer::manager_thread(void * filename)
   return write_errors;
 }
 
-msg_t SampleBuffer::write_thread(void *)
+msg_t SampleBuffer::write_thread(uint8_t *start_buffer)
 {
   chRegSetThreadName("write");
   UINT bytes_written = 0;
   msg_t write_errors = 0;
-  uint8_t buffer_to_write = 0;
+  uint8_t buffer_to_write = *start_buffer;
 
   while (1) {
-    chMtxLock(&buffer_mtx_);
-      bool buffer_filled = (active_buffer_ != buffer_to_write);
-    chMtxUnlock();
-    if (buffer_filled) {
-      FRESULT res = f_write(&f_, &buffer_[buffer_to_write][0],
-                            bytes_per_buffer_, &bytes_written);
-      if ((res != FR_OK) || (bytes_written != bytes_per_buffer_))
-        ++write_errors;
-
-      buffer_to_write = (buffer_to_write + 1) % number_of_buffers_;
-    } else {
-      chThdSleep(MS2ST(10));
+    if (chSemWaitTimeout(&buffer_sem_, write_thread_sleep_time_) != RDY_OK) {
       if (chThdShouldTerminate())
         break;
+      continue;
     }
+    FRESULT res = f_write(&f_, &buffer_[buffer_to_write][0],
+                          bytes_per_buffer_, &bytes_written);
+    write_errors += (res != FR_OK) || (bytes_written != bytes_per_buffer_);
+    buffer_to_write = (buffer_to_write + 1) % number_of_buffers_;
   }
   chThdExit(write_errors);
   return write_errors;
--- firmware/src/SampleBuffer_priv.h
+++ firmware/src/SampleBuffer_priv.h
@@ -4,13 +4,13 @@
 inline
 msg_t SampleBuffer::write_thread_(void * arg)
 {
-  return SampleBuffer::Instance().write_thread(arg);
+  return SampleBuffer::Instance().write_thread(static_cast<uint8_t*>(arg));
 }
 
 inline
 msg_t SampleBuffer::manager_thread_(void * arg)
 {
-  return SampleBuffer::Instance().manager_thread(arg);
+  return SampleBuffer::Instance().manager_thread(static_cast<char *>(arg));
 }
 
 // Caller: control_thread_
@@ -18,6 +18,12 @@ inline
 msg_t SampleBuffer::insert(Sample & s)
 {
   return chMsgSend(tp_manage_, reinterpret_cast<msg_t>(&s));
+}
+
+inline
+bool SampleBuffer::buffer_overflow()
+{
+  return chSemGetCounterI(&buffer_sem_) > number_of_buffers_ - 2;
 }
 
 #endif
