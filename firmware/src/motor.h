dab210014a99a4ef9d76bb2fe86716f1ebe86e8c
Dale Lukas Peterson <hazelnusse@gmail.com>
2013-05-16 14:45:46 -0700

    Implement Motor class.

---
+++ firmware/src/motor.h
@@ -0,0 +1,54 @@
+#ifndef MOTOR_H
+#define MOTOR_H
+
+#include <cstdint>
+#include "hal.h"
+
+class MotorStatus {
+ public:
+  bool direction;
+  bool enable;
+  bool fault;
+};
+
+class Motor {
+ public:
+  Motor(GPIO_TypeDef * gpio_port,
+        uint8_t direction,
+        uint8_t enable,
+        uint8_t fault,
+        stm32_tim_t * pwm_timer,
+        uint8_t ccr_channel,
+        float max_current,          // Amps
+        float torque_constant,      // Newton meters per amp
+        bool reverse_polarity = false);
+  ~Motor();
+
+  bool set_torque(float torque);
+
+  void disable();
+  void enable();
+
+ private:
+  uint32_t current_to_ccr(float current) const;
+  void set_ccr(uint32_t ccr);
+  void set_direction_negative();
+  void set_direction_positive();
+
+  GPIO_TypeDef * const gpio_port_;
+  stm32_tim_t * const pwm_timer_;
+  const float inv_torque_constant_; // Amps per Newton meter
+  const float max_current_;
+  const float max_torque_;
+  const uint8_t direction_pin_;
+  const uint8_t enable_pin_;
+  const uint8_t fault_pin_;
+  const uint8_t ccr_channel_;
+  const uint8_t dir_negative_;
+  const uint8_t dir_positive_;
+};
+
+#include "motor_priv.h"
+
+#endif
+
---
+++ firmware/src/motor_priv.h
@@ -0,0 +1,105 @@
+#ifndef MOTOR_PRIV_H
+#define MOTOR_PRIV_H
+
+#include "bitband.h"
+#include "constants.h"
+
+Motor::Motor(GPIO_TypeDef * gpio_port,
+             uint8_t direction_pin,
+             uint8_t enable_pin,
+             uint8_t fault_pin,
+             stm32_tim_t * pwm_timer,
+             uint8_t ccr_channel,
+             float max_current,          // Amps
+             float torque_constant,      // Newton meters per amp
+             bool reverse_polarity)
+  : gpio_port_(gpio_port),
+    pwm_timer_(pwm_timer),
+    inv_torque_constant_(1.0f / torque_constant),
+    max_current_(max_current),
+    max_torque_(max_current * torque_constant),
+    direction_pin_(direction_pin),
+    enable_pin_(enable_pin),
+    fault_pin_(fault_pin),
+    ccr_channel_(ccr_channel),
+    dir_negative_(reverse_polarity ? 1 : 0),
+    dir_positive_(reverse_polarity ? 0 : 1)
+{
+//  if (reverse_polarity) {
+//    dir_negative_ = 1;
+//    dir_positive_ = 0;
+//  }
+  set_ccr(0);              
+  set_direction_positive();
+  enable();
+}
+
+Motor::~Motor()
+{
+  disable();
+  set_direction_positive();
+  set_ccr(0);
+}
+
+inline
+bool Motor::set_torque(float torque)
+{
+  bool saturated = false;
+  if (torque < 0.0f) {
+    set_direction_negative();
+    torque = -torque;
+  } else {
+    set_direction_positive();
+  }
+
+  if (torque > max_torque_) {
+    torque = max_torque_;
+    saturated = true;
+  }
+
+  set_ccr(current_to_ccr(torque * inv_torque_constant_));
+  return saturated;
+}
+
+inline
+void Motor::disable()
+{
+  MEM_ADDR(BITBAND(reinterpret_cast<uint32_t>(&(gpio_port_->ODR)),
+                   enable_pin_)) = 1;
+}
+
+inline
+void Motor::enable()
+{
+  MEM_ADDR(BITBAND(reinterpret_cast<uint32_t>(&(gpio_port_->ODR)),
+                   enable_pin_)) = 0;
+}
+
+inline
+uint32_t Motor::current_to_ccr(float current) const
+{
+  return static_cast<uint32_t>((((constants::PWM_ARR + 1) / max_current_)) * current);
+}
+
+inline
+void Motor::set_ccr(uint32_t ccr)
+{
+  pwm_timer_->CCR[ccr_channel_] = ccr;
+}
+
+inline 
+void Motor::set_direction_negative()
+{
+  MEM_ADDR(BITBAND(reinterpret_cast<uint32_t>(&(gpio_port_->ODR)),
+                   direction_pin_)) = dir_negative_;
+}
+
+inline 
+void Motor::set_direction_positive()
+{
+  MEM_ADDR(BITBAND(reinterpret_cast<uint32_t>(&(gpio_port_->ODR)),
+                   direction_pin_)) = dir_positive_;
+}
+
+#endif
+
