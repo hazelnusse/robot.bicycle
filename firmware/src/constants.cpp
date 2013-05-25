c5c49df22f58ab1cc80799559c20b7e38410dc2b
Dale Lukas Peterson <hazelnusse@gmail.com>
2013-05-25 02:42:58 -0700

    Add a few constants and change a few to constexpr.

--- firmware/src/constants.cpp
+++ firmware/src/constants.cpp
@@ -15,13 +15,30 @@ const float g = 9.81f;
 const uint32_t PWM_ARR = 0xFFFE;
 const uint32_t ENC_ARR = 0xFFFF;
 
-const uint32_t T_ms = 5;
-const uint32_t RW_N = 10;
+// const uint8_t loop_period_ms = 5;
+const float system_timer_frequency = 4.0e6f;
+const float system_timer_period = 1.0f / system_timer_frequency;
 const uint32_t YC_N = 1;
 
 const float Nm_per_ozfin = 0.00706155182175f;
 
 const float wheel_radius = 0.3359f;
+
+// Accelerometer constants
+const float accelerometer_sensitivity = g / 16384.0f;
+const float gyroscope_sensitivity = rad_per_degree / 131.0f;
+const float thermometer_offset = 36.53f;
+const float thermometer_sensitivity = 1.0f / 340.0f;
+
+const float gyro_x_bias = -0.12831133410801182f;
+const float gyro_y_bias = 0.032857218962598515f;
+const float gyro_z_bias = 0.010641128707006363f;
+const float dcm[6] = {-0.894519492243436f,
+                      -0.0635181679465503f,
+                      0.0608731305741522f,
+                      0.446568482938378f,
+                      0.997939373875708f,
+                      0.0202846750691697f};
 
 }
 
--- firmware/src/constants.h
+++ firmware/src/constants.h
@@ -17,13 +17,28 @@ extern const float g;
 extern const uint32_t PWM_ARR;
 extern const uint32_t ENC_ARR;
 
-extern const uint32_t T_ms;  // main loop update rate in ms
-extern const uint32_t RW_N;  // rear wheel control update prescalar
+constexpr uint8_t loop_period_ms = 5;  // main loop update rate in ms
+extern const float system_timer_frequency;
+extern const float system_timer_period;
+// TODO: Move to fork motor controller
 extern const uint32_t YC_N;  // yaw rate control update prescalar
 
 extern const float Nm_per_ozfin;
 
 extern const float wheel_radius;
+
+extern const float accelerometer_sensitivity;
+extern const float gyroscope_sensitivity;
+extern const float thermometer_offset;
+extern const float thermometer_sensitivity;
+
+extern const float gyro_x_bias;
+extern const float gyro_y_bias;
+extern const float gyro_z_bias;
+extern const float dcm[6];
+
+// Unity gain low pass filter
+constexpr float lpf_pole = -100.0f;  // 1 / (s/-100 + 1)
 
 } // namespace constants
 
