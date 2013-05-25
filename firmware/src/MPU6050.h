a7ffc9a752bef027cd6e078c74aa6c62bb9de00d
Dale Lukas Peterson <hazelnusse@gmail.com>
2013-05-25 02:48:32 -0700

    Made MPU6050 a normal non Singleton and clean up.

--- firmware/src/MPU6050.h
+++ firmware/src/MPU6050.h
@@ -8,33 +8,29 @@
 #include "Singleton.h"
 #include "sample.pb.h"
 
-class MPU6050 : public Singleton<MPU6050> {
-  friend class Singleton<MPU6050>;
+class MPU6050 {
  public:
-  bool Acquire(Sample & s) const;
-  bool Initialize(I2CDriver * i2c);
-  void DeInitialize();
+  MPU6050();
+  ~MPU6050();
+
+  bool acquire_data(Sample & s) const;
+  bool is_initialized() const { return initialized_; }
+
   static float phi_dot(const Sample & s);
   static float psi_dot(const Sample & s);
   static void convertData(Sample & s, int16_t ar[7]);
 
  private:
-  MPU6050();
-  MPU6050(const MPU6050&) = delete;
-  MPU6050 & operator=(const MPU6050&) = delete;
+  bool initialize();
 
   static bool checkTransmission(msg_t res, Sample & s);
-
-  static const float gyro_x_bias;
-  static const float gyro_y_bias;
-  static const float gyro_z_bias;
-  static const float dcm[6];
 
   I2CDriver * i2c_;
   I2CConfig i2cfg_;
   systime_t timeout_;
   uint8_t I2C_ADDR;
   uint8_t ACCEL_XOUT_ADDR;
+  bool initialized_;
 };
 #endif
 
