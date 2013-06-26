ad626f5d6bd960bf267382218b4271a0f3b2b599
Dale Lukas Peterson <hazelnusse@gmail.com>
2013-06-25 19:22:43 -0700

    Fix bug in observer A matrix.
    Previous commit had observer A matrix equal to plant A matrix, instead of
    A-K_obs*C. This results in a observer that has unstable dynamics since the
    plant dynamics are unstable.

--- design/control_design_functions.cc
+++ design/control_design_functions.cc
@@ -126,18 +126,18 @@ void compute_observer_gains(const design_parameters & params,
   poles(2, 0) = poles(0, 0) - 0.4;
   poles(3, 0) = poles(0, 0) - 0.6;
   matlab.put_Matrix(poles, "poles");
-  matlab.eval("[K_obs, prec, message] = place(A', C_obs', poles); K_obs = -K_obs';");
+  matlab.eval("[K_obs, prec, message] = place(A', C_obs', poles); K_obs = K_obs';");
   data.K_obs = matlab.get_Matrix("K_obs");
   std::cout << "K_obs = \n" << data.K_obs << std::endl;
-  Eigen::EigenSolver<Eigen::MatrixXd> es(data.A + data.K_obs * C_obs);
+  Eigen::EigenSolver<Eigen::MatrixXd> es(data.A - data.K_obs * C_obs);
   std::cout << "Observer CL evals = \n" << es.eigenvalues().transpose() << std::endl;
-  matlab.eval("B_obs = [K_obs, B]; sys_obs_c = ss(A, B_obs, eye(4), zeros(4, 3));"
+  matlab.eval("B_obs = [K_obs, B]; sys_obs_c = ss(A - K_obs * C_obs, B_obs, eye(4), zeros(4, 3));"
               "sys_obs_d = c2d(sys_obs_c, Ts, 'tustin'); A_obs_d = sys_obs_d.A; B_obs_d = sys_obs_d.B;");
   data.A_obs_d = matlab.get_Matrix("A_obs_d");
   data.B_obs_d = matlab.get_Matrix("B_obs_d");
   std::cout << "Observer A matrix:\n" << data.A_obs_d << std::endl;
   std::cout << "Observer B matrix:\n" << data.B_obs_d << std::endl;
-  Eigen::EigenSolver<Eigen::MatrixXd> es_d(data.A_obs_d + data.B_obs_d.block(0, 0, 4, 2) * C_obs);
+  Eigen::EigenSolver<Eigen::MatrixXd> es_d(data.A_obs_d);
   std::cout << "Observer A_d evals:\n" << es_d.eigenvalues() << std::endl;
 }
 
